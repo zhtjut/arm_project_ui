e9cb0aa1d892ddafc39bbd3b99c51a64e4c2a4dd
tj621 <wangyuanxv123@163.com>
2016-05-31 12:57:32 +0800

    second

---
+++ src/com/Data.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Data {
+    private static String out_radiation="20000";
+    private static String out_temprature="25";
+    private static String out_co2="460";
+    private static String out_humidity="60";
+    private static String out_windspeed="20";
+    private static String in_temprature="25";
+    private static String in_humidity="25";
+    private static String in_ratiation="10000";
+    private static String in_co2="420";
+
+    public static String getOut_radiation() {
+        return out_radiation;
+    }
+
+    public static void setOut_radiation(String aOut_radiation) {
+        out_radiation = aOut_radiation;
+    }
+
+    public static String getOut_temprature() {
+        return out_temprature;
+    }
+
+    public static void setOut_temprature(String aOut_temprature) {
+        out_temprature = aOut_temprature;
+    }
+
+    public static String getOut_co2() {
+        return out_co2;
+    }
+
+    public static void setOut_co2(String aOut_co2) {
+        out_co2 = aOut_co2;
+    }
+
+    public static String getOut_humidity() {
+        return out_humidity;
+    }
+
+    public static void setOut_humidity(String aOut_humidity) {
+        out_humidity = aOut_humidity;
+    }
+
+    public static String getOut_windspeed() {
+        return out_windspeed;
+    }
+
+    public static void setOut_windspeed(String aOut_windspeed) {
+        out_windspeed = aOut_windspeed;
+    }
+
+    public static String getIn_temprature() {
+        return in_temprature;
+    }
+
+    public static void setIn_temprature(String aIn_temprature) {
+        in_temprature = aIn_temprature;
+    }
+
+    public static String getIn_humidity() {
+        return in_humidity;
+    }
+
+    public static void setIn_humidity(String aIn_humidity) {
+        in_humidity = aIn_humidity;
+    }
+
+    public static String getIn_ratiation() {
+        return in_ratiation;
+    }
+
+    public static void setIn_ratiation(String aIn_ratiation) {
+        in_ratiation = aIn_ratiation;
+    }
+
+    public static String getIn_co2() {
+        return in_co2;
+    }
+
+    public static void setIn_co2(String aIn_co2) {
+        in_co2 = aIn_co2;
+    }
+
+   
+    
+    
+}
